6c4e36bf86ba3b3a66018037d96b21d6077c3939
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-02-10 18:17:09 -0300

    feat - operadores aritmetcos

--- web_fundamentals/script.js
+++ web_fundamentals/script.js
@@ -13,3 +13,16 @@ let age2 = 33; // number
 let isMarried2 = true; // boolean
 let isAdmin = null; // null
 let user; // undefined
+
+// Operadores aritméticos:
+let n1 = 2;
+let n2 = 10;
+
+let sum = n1 + n2; // soma de n1 e n2;
+let sub = n1 - n2; // subtração de n1 por n2;
+let mult = n1 * n2; // multiplicação de n1 por n2;
+let div = n1 / n2; // divisão de n1 por n2;
+let mod = n1 % n2; // resto da divisão de n1 por n2;
+let exp = n1 ** n2; // 2 elevado a 10
+let inc =  ++n1; // n1 = n1 + 1;
+let dec = --n2; // n2 = n2 - 1;
