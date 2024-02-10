a453445dfbde9af455b7ea49426917ede1a9c0b3
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-02-10 18:11:07 -0300

    feat - tipos primitivos

--- web_fundamentals/script.js
+++ web_fundamentals/script.js
@@ -6,3 +6,10 @@ let name = 'Yeshua';
 let age = 33;
 let isMarried = false;
 
+
+// Tipos primitivos:
+let name2 = 'Elielson'; // string
+let age2 = 33; // number
+let isMarried2 = true; // boolean
+let isAdmin = null; // null
+let user; // undefined
